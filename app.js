ab54f0ddee5b1086ade34aa3b59f61d87982d19c
unknown <Dario Araya>
2021-10-25 02:19:17 -0300

    se agrego npm start

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.get('/contacto', (req, res) => {
 });
 */
 
-app.listen(3000,()=>{
-    console.log('Server on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server on port ${port}`);
 });
 
